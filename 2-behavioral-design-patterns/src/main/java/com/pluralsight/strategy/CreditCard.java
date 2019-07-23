ac31c4919da6379de19c9f4e629bfafc7459f54e
josebatista <wyldkatpt@users.noreply.github.com>
2019-07-23 23:21:18 +0100

    Added strategy skeleton

---
+++ 2-behavioral-design-patterns/src/main/java/com/pluralsight/strategy/CreditCard.java
@@ -0,0 +1,35 @@
+package com.pluralsight.strategy;
+
+public class CreditCard {
+
+    private String number;
+    private String date;
+    private String cvv;
+
+    public CreditCard() {
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+}
---
+++ 2-behavioral-design-patterns/src/main/java/com/pluralsight/strategy/strategy/ValidationStrategy.java
@@ -0,0 +1,5 @@
+package com.pluralsight.strategy.strategy;
+
+public interface ValidationStrategy {
+
+}
